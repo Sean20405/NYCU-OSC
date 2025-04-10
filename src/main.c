5a5b1dcfc46f2555b9b9e3096d97cc140536151b
Sean20405 <sean20405.cs11@nycu.edu.tw>
2025-04-10 12:49:52 +0800

    init: upload Lab3

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include "uart.h"
+#include "shell.h"
+#include "devicetree.h"
+#include "timer.h"
+
+void main() {
+    // Get the address of the device tree blob
+    uint32_t dtb_address;
+    asm volatile ("mov %0, x20" : "=r" (dtb_address));
+
+    int ret = fdt_init((void*)dtb_address);
+    if (ret) {
+        uart_puts("Failed to initialize the device tree blob!\n");
+        return;
+    }
+    ret = fdt_traverse(initramfs_callback);
+    if (ret) {
+        uart_puts("Failed to traverse the device tree blob!\n");
+        return;
+    }
+
+    enable_irq_el1();
+
+    add_timer(print_uptime, NULL, 2 * get_freq());
+
+    shell();
+}
